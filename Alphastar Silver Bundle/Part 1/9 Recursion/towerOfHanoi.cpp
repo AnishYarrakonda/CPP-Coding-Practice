e4dd9a1f2871fdeaa9a52a4316a39d0c2b1d1cfc
Anish Yarrakonda <anish.yarrakonda456@gmail.com>
2026-01-06 09:05:25 -0800

    sdf

--- Alphastar Silver Bundle/Part 1/9 Recursion/towerOfHanoi.cpp
+++ Alphastar Silver Bundle/Part 1/9 Recursion/towerOfHanoi.cpp
@@ -1,44 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// basic types
-using ll = long long;
-using db = double;
-using str = string;
+void hanoi(int n, int from, int aux, int to) {
+    if (n == 0) return;
 
-// generic templates
-template<typename A, typename B> using p = pair<A,B>;               // Pair
-template<typename T> using v = vector<T>;                           // 1D vector
-template<typename T> using vv = vector<vector<T>>;                  // 2D vector
-template<typename T> using uset = unordered_set<T>;                 // unordered set
-template<typename K, typename V> using umap = unordered_map<K,V>;   // unordered map
+    // Move n-1 disks from 'from' to 'aux'
+    hanoi(n - 1, from, to, aux);
 
-// even shorter shorthands for data types dealing with int
-using pi = pair<int,int>;
-using vi = vector<int>;
-using vvi = vector<vi>;
+    // Move largest disk from 'from' to 'to'
+    cout << from << " --> " << to << "\n";
 
-// macros
-#define rep(i,a,b) for(int i = a; i < (b); i++)     // for loop
-#define all(x) (x).begin(),(x).end()                // entire container
-
-// simple functions
-template<typename T> bool ckmin(T &a, T b) { return b < a ? a=b, 1 : 0; }                   // update a to min of (a, b)
-template<typename T> bool ckmax(T &a, T b) { return b > a ? a=b, 1 : 0; }                   // update a to max of (a, b)
-template<typename T> T minv(const v<T>& vec) { return *min_element(all(vec)); }             // min of vector
-template<typename T> T maxv(const v<T>& vec) { return *max_element(all(vec)); }             // max of vector
-template<typename T> T sum(const v<T>& vec) { return accumulate(all(vec), T(0)); }          // sum of vector
-template<typename T> int countv(const v<T>& vec, T val) { return count(all(vec), val); }    // count occurences
-
-// debugging (print 1D and 2D vectors to console)
-template<typename T> void prettyprint(const v<T>& vec) { for(auto &x : vec) cout << x << ' '; cout << '\n'; }
-template<typename T> void prettyprint(const vv<T>& mat) { for(auto &row : mat) prettyprint(row); }
-
-// Solution Code below
+    // Move n-1 disks from 'aux' to 'to'
+    hanoi(n - 1, aux, from, to);
+}
 
 int main() {
-    
+    int N;
+    cin >> N;
 
+    // Move N disks from rod 1 to rod 3 using rod 2
+    hanoi(N, 1, 2, 3);
 
     return 0;
 }
