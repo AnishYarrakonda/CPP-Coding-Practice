2225f299585090247de166384d31f85949b4e00d
Anish Yarrakonda <anish.yarrakonda456@gmail.com>
2026-01-08 09:49:56 -0800

    good

--- Alphastar Silver Bundle/Part 2/3 Floodfill/worldDomination.cpp
+++ Alphastar Silver Bundle/Part 2/3 Floodfill/worldDomination.cpp
@@ -30,12 +30,51 @@ template<typename T> void prettyprintnewline(const v<T>& vec) { for(auto &x : ve
 // Solution Code below
 int N;
 
-pi bestContinent = {0, 1e9};
+bool inBounds(int x, int y) {
+    return (0 <= x && x < N && 0 <= y && y < N);
+}
+
+pi bestContinent = {0, -1e9};
+
+vi dx = {1, -1, 0, 0};
+vi dy = {0, 0, 1, -1};
 
 void floodfill(vvi& grid, vvi& visited) {
     rep(i,0,N) {
         rep(j,0,N) {
-            
+            if (grid[i][j] && !visited[i][j]) {
+                queue<pi> q;
+                q.push({i, j});
+                visited[i][j] = 1;
+
+                int area = 0;
+                int perimeter = 0;
+
+                while (!q.empty()) {
+                    auto [a, b] = q.front();
+                    q.pop();
+
+                    area++;
+                    
+                    rep(k,0,4) {
+                        int x = a + dx[k];
+                        int y = b + dy[k];
+
+                        if (inBounds(x, y) && grid[x][y]) {
+                            if (!visited[x][y]) {
+                                q.push({x, y});
+                                visited[x][y] = 1;
+                            }
+                        } else {
+                            perimeter++;
+                        }
+                    }
+                }
+                pi continent = make_pair(area, -perimeter);
+                if (continent > bestContinent) {
+                    bestContinent = continent;
+                }
+            }
         }
     }
 }
@@ -54,7 +93,9 @@ int main() {
     }
     vvi visited(N, vi(N, 0));
 
+    floodfill(grid, visited);
 
+    cout << bestContinent.first << " " << -bestContinent.second << endl;
 
     return 0;
 }
