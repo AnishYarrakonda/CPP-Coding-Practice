a75f901e1a88608561dc55acaed0e5cbe8e76030
Anish Yarrakonda <number1noob1234@gmail.com>
2026-01-09 15:45:00 -0800

    sdfds

---
+++ Alphastar Silver Bundle/Part 2/4 Data Structures/acornFarm.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// basic types
+using ll = long long;
+using db = double;
+using str = string;
+
+// generic templates
+template<typename A, typename B> using p = pair<A,B>;               // Pair
+template<typename T> using v = vector<T>;                           // 1D vector
+template<typename T> using vv = vector<vector<T>>;                  // 2D vector
+template<typename T> using uset = unordered_set<T>;                 // unordered set
+template<typename K, typename V> using umap = unordered_map<K,V>;   // unordered map
+
+// even shorter shorthands for data types dealing with int
+using pi = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+// macros
+#define rep(i,a,b) for(int i = a; i < (b); i++)     // for loop
+#define all(x) (x).begin(),(x).end()                // entire container
+
+// debugging (print 1D and 2D vectors to console)
+template<typename T> void prettyprint(const v<T>& vec) { for(auto &x : vec) cout << x << ' '; cout << '\n'; }
+template<typename T> void prettyprint(const vv<T>& mat) { for(auto &row : mat) prettyprint(row); }
+template<typename T> void prettyprintnewline(const v<T>& vec) { for(auto &x : vec) cout << x << '\n'; }
+
+// Solution Code below
+
+int main() {
+    int N, A; cin >> N >> A;
+    unordered_map<int,ll> acorns;
+    unordered_set<int> bestID = {-1};
+    ll mostAcorns = -1e18;
+
+    multiset<p<int,pi>> events;
+
+    rep(i,0,N) {
+        int day, ID, change;
+        cin >> day >> ID >> change;
+        events.insert({day, {ID, change}});
+    }
+
+    int ribbons = 0;
+    
+    
+    return 0;
+}
