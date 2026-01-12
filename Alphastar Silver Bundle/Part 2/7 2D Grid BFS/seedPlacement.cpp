42c68066a7f528d425b78420c660c8c4c24535d4
Anish Yarrakonda <number1noob1234@gmail.com>
2026-01-11 20:45:38 -0800

    w

--- Alphastar Silver Bundle/Part 2/7 2D Grid BFS/seedPlacement.cpp
+++ Alphastar Silver Bundle/Part 2/7 2D Grid BFS/seedPlacement.cpp
@@ -1,42 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// basic types
-using ll = long long;
-using db = double;
-using str = string;
-
-// generic templates
-template<typename A, typename B> using p = pair<A,B>;               // Pair
-template<typename T> using v = vector<T>;                           // 1D vector
-template<typename T> using vv = vector<vector<T>>;                  // 2D vector
-template<typename T> using uset = unordered_set<T>;                 // unordered set
-template<typename K, typename V> using umap = unordered_map<K,V>;   // unordered map
-
-// even shorter shorthands for data types dealing with int
 using pi = pair<int,int>;
 using vi = vector<int>;
-using vvi = vector<vi>;
 
-// macros
-#define rep(i,a,b) for(int i = a; i < (b); i++)     // for loop
-#define all(x) (x).begin(),(x).end()                // entire container
-
-// debugging (print 1D and 2D vectors to console)
-template<typename T> void prettyprint(const v<T>& vec) { for(auto &x : vec) cout << x << ' '; cout << '\n'; }
-template<typename T> void prettyprint(const vv<T>& mat) { for(auto &row : mat) prettyprint(row); }
-template<typename T> void prettyprintnewline(const v<T>& vec) { for(auto &x : vec) cout << x << '\n'; }
-
-// Solution Code below
+#define rep(i,a,b) for(int i = a; i < (b); i++)
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int N;
     cin >> N;
-    
-    vvi grid(1000, vi(1000, 0));
 
-    rep(i,0,N) {
-        
+    set<pi> seeds;
+    map<pi,int> deg;
+    queue<pi> q;
+
+    int ANS = 0;
+
+    vi dx = {1, -1, 0, 0};
+    vi dy = {0, 0, 1, -1};
+
+    rep(_, 0, N) {
+        int x, y;
+        cin >> x >> y;
+        pi coords = {x, y};
+
+        if (seeds.count(coords)) {
+            ANS--;
+            cout << ANS << '\n';
+            continue;
+        }
+
+        seeds.insert(coords);
+        deg[coords] = 0;
+
+        rep(k, 0, 4) {
+            pi adj = {x + dx[k], y + dy[k]};
+            if (seeds.count(adj)) {
+                deg[coords]++;
+                deg[adj]++;
+                if (deg[adj] == 3) q.push(adj);
+            }
+        }
+
+        if (deg[coords] == 3) q.push(coords);
+
+        while (!q.empty()) {
+            auto [a, b] = q.front();
+            q.pop();
+
+            if (deg[{a, b}] != 3) continue;
+
+            rep(k, 0, 4) {
+                pi nxt = {a + dx[k], b + dy[k]};
+                if (!seeds.count(nxt)) {
+                    seeds.insert(nxt);
+                    deg[nxt] = 0;
+                    ANS++;
+
+                    rep(d, 0, 4) {
+                        pi adj = {nxt.first + dx[d], nxt.second + dy[d]};
+                        if (seeds.count(adj)) {
+                            deg[nxt]++;
+                            deg[adj]++;
+                            if (deg[adj] == 3) q.push(adj);
+                        }
+                    }
+
+                    if (deg[nxt] == 3) q.push(nxt);
+                    break;
+                }
+            }
+        }
+
+        cout << ANS << '\n';
     }
 
     return 0;
