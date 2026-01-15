afcaf294508ac4e596fbb249a272f291f7c736d4
Anish Yarrakonda <anish.yarrakonda456@gmail.com>
2026-01-15 10:32:23 -0800

    fdsf

---
+++ Alphastar Silver Bundle/Part 2/9 Disjoint Cycles/bookArrangement.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// basic types
+using ll = long long;
+using db = double;
+using str = string;
+
+// generic templates
+template<typename A, typename B> using p = pair<A,B>;               // Pair
+template<typename T> using v = vector<T>;                           // 1D vector
+template<typename T> using vv = vector<vector<T>>;                  // 2D vector
+template<typename T> using uset = unordered_set<T>;                 // unordered set
+template<typename K, typename V> using umap = unordered_map<K,V>;   // unordered map
+
+// even shorter shorthands for data types dealing with int
+using pi = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+// macros
+#define rep(i,a,b) for(int i = a; i < (b); i++)     // for loop
+#define all(x) (x).begin(),(x).end()                // entire container
+
+// debugging (print 1D and 2D vectors to console)
+template<typename T> void prettyprint(const v<T>& vec) { for(auto &x : vec) cout << x << ' '; cout << '\n'; }
+template<typename T> void prettyprint(const vv<T>& mat) { for(auto &row : mat) prettyprint(row); }
+template<typename T> void prettyprintnewline(const v<T>& vec) { for(auto &x : vec) cout << x << '\n'; }
+
+// Solution Code below
+
+int main() {
+    int N; cin >> N;
+    vi books(N); rep(i,0,N) cin >> books[i];
+    vi sortedBooks(books); sort(all(sortedBooks));
+    umap<int,int> points;
+    umap<int,int> visited;
+    rep(i,0,N) {points[sortedBooks[i]] = books[i]; visited[books[i]] = 0;}
+
+    v<uset<int>> cycles;
+
+    rep(i,0,N) {
+        if (visited[books[i]]) continue;
+
+        int currNode = books[i];
+        visited[currNode] = 1;
+        bool b = false;
+        uset<int> cycle = {currNode};
+
+        while (true) {
+            if (visited[currNode] && b) break;
+        }
+    }
+
+    return 0;
+}
