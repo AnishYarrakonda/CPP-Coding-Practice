9074978bb70d49d891be442481a3d64c36d56799
Anish Yarrakonda <number1noob1234@gmail.com>
2026-01-14 21:44:15 -0800

    s

--- Alphastar Silver Bundle/Part 2/10 Mixed Problem Set/acornDispensing.cpp
+++ Alphastar Silver Bundle/Part 2/10 Mixed Problem Set/acornDispensing.cpp
@@ -37,8 +37,64 @@ int main() {
     rep(i,0,M) {
         int x, y, a, b;
         cin >> x >> y >> a >> b;
-        switches[x][y].push_back({a, b});
+        switches[x-1][y-1].push_back({a-1, b-1});
     }
+
+    vvi on(N, vi(N, 0));
+    on[0][0] = 1;
+
+    vvi used(N, vi(N, 0));
+
+    for (auto [u, v] : switches[0][0]) on[u][v] = 1;
+    used[0][0] = 1;
+
+    vi dx = {1, -1, 0, 0};
+    vi dy = {0, 0, 1, -1};
+
+    bool changed = true;
+
+    while (changed) {
+        changed = false;
+
+        queue<pi> q;
+        q.push({0, 0});
+
+        vvi visited(N, vi(N, 0));
+        visited[0][0] = 1;
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            rep(i,0,4) {
+                int a = x + dx[i];
+                int b = y + dy[i];
+
+                if (a >= N || a < 0 ||
+                    b >= N || b < 0) continue;
+
+                if (visited[a][b]) continue;
+
+                if (!on[a][b]) continue;
+                
+                if (!used[a][b]) {
+                    for (auto [c, d] : switches[a][b]) {
+                        on[c][d] = 1;
+                    }
+                    used[a][b] = 1;
+                    changed = true;
+                }
+
+                q.push({a, b});
+                visited[a][b] = 1;
+            }
+        }
+    }
+
+    int total = 0;
+    rep(i,0,N) rep(j,0,N) total += on[i][j];
+
+    cout << total << endl;
 
     return 0;
 }
