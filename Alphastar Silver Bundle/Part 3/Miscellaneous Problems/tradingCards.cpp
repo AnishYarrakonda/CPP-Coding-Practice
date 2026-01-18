790c5c1c3a066fdb7746df0e63dd54e8c71aca04
Anish Yarrakonda <number1noob1234@gmail.com>
2026-01-18 00:36:05 -0800

    ew

--- Alphastar Silver Bundle/Part 3/Miscellaneous Problems/tradingCards.cpp
+++ Alphastar Silver Bundle/Part 3/Miscellaneous Problems/tradingCards.cpp
@@ -1,36 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// basic types
-using ll = long long;
-using db = double;
-using str = string;
-
-// generic templates
-template<typename A, typename B> using p = pair<A,B>;               // Pair
-template<typename T> using v = vector<T>;                           // 1D vector
-template<typename T> using vv = vector<vector<T>>;                  // 2D vector
-template<typename T> using uset = unordered_set<T>;                 // unordered set
-template<typename K, typename V> using umap = unordered_map<K,V>;   // unordered map
-
-// even shorter shorthands for data types dealing with int
-using pi = pair<int,int>;
-using vi = vector<int>;
-using vvi = vector<vi>;
-
-// macros
-#define rep(i,a,b) for(int i = a; i < (b); i++)     // for loop
-#define all(x) (x).begin(),(x).end()                // entire container
-
-// debugging (print 1D and 2D vectors to console)
-template<typename T> void prettyprint(const v<T>& vec) { for(auto &x : vec) cout << x << ' '; cout << '\n'; }
-template<typename T> void prettyprint(const vv<T>& mat) { for(auto &row : mat) prettyprint(row); }
-template<typename T> void prettyprintnewline(const v<T>& vec) { for(auto &x : vec) cout << x << '\n'; }
-
-// Solution Code below
-
 int main() {
-    
+    int N, K, L;
+    cin >> N >> K >> L;
 
-    return 0;
+    vector<int> A(N);
+    for (int &x : A) cin >> x;
+    sort(A.rbegin(), A.rend());
+
+    auto can = [&](int r) {
+        long long need = 0;
+        for (int i = 0; i < r; i++) {
+            if (A[i] + K < r) return false;
+            need += max(0, r - A[i]);
+        }
+        return need <= (long long)K * L;
+    };
+
+    int lo = 0, hi = N;
+    while (lo < hi) {
+        int mid = (lo + hi + 1) / 2;
+        if (can(mid)) lo = mid;
+        else hi = mid - 1;
+    }
+
+    cout << lo << "\n";
 }
